4d2c719080f54fa48c00aa3f19355bfeb56a277f
Warren Zhang <warrenzhang17@gmail.com>
2023-03-13 19:44:56 -0400

    cats

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name) {
+    const newcats = [...cats, name]
+    return newcats
+}
+
+function prependCat(name) {
+    const newcats = [name, ...cats]
+    return newcats
+}
+
+function removeLastCat(name) {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(name) {
+    return cats.slice(1)
+}
